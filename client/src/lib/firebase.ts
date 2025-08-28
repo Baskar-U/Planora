8a524e3292397e0329dd676cf0353b0215dd1bff
Baskar-U <baskar.26csa@licet.ac.in>
2025-08-28 13:40:19 +0530

    Final

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBHdkoD3yYCe9gLCs8zV9_QpL7f0P34ag",
-  authDomain: "planora-ce3a5.firebaseapp.com",
-  projectId: "planora-ce3a5",
-  storageBucket: "planora-ce3a5.appspot.com",
-  messagingSenderId: "681139972771",
-  appId: "1:681139972771:web:5e17d10a3549c1abe52d16",
-  measurementId: "G-HTHPBGY2GQ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
